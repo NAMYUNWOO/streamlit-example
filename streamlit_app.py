5fce542fe263fd5a6797f6da18ebb413823c7db8
namyunwoo <namyunwoo@icloud.com>
2024-02-29 21:09:40 +0900

    nexon api test

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,29 +13,13 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
+api_key = st.secrets['NEXON_API_KEY']
+headers = {
+  "x-nxopen-api-key": api_key
+}
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+characterName = "아델"
+urlString = "https://open.api.nexon.com/heroes/v1/id?character_name=" + characterName
+response = requests.get(urlString, headers = headers)
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+st.write(str(response.json()))
