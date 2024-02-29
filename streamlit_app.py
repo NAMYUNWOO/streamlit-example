bb5aad4fe007fb60828b7a0370087e9f115b5369
namyunwoo <namyunwoo@icloud.com>
2024-02-29 21:30:18 +0900

    nexon api test

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,14 +6,7 @@ import requests
 from datetime import datetime,timedelta
 
 """
-test from namyunwoo
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+🍁
 """
 api_key = st.secrets['NEXON_API_KEY']
 headers = {
