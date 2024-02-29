26f72ca380ea8f281198c1607569c56bccd5c4ab
namyunwoo <namyunwoo@icloud.com>
2024-03-01 02:38:22 +0900

    nexon api test

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,18 +40,18 @@ widget_id = (id for id in range(1, 100_00))
 
 col1, col2 = st.columns(2)
 with col1:
-    characterName = st.text_input(label='캐릭터명1', placeholder ='캐릭터명을 입력하고 엔터', key=next(widget_id))
+    characterName = st.text_input(label='캐릭터명1',value='캐릭터명을 입력하고 엔터', placeholder ='캐릭터명을 입력하고 엔터', key=next(widget_id))
 
 with col2:
-    characterName2 = st.text_input(label='캐릭터명2', placeholder ='캐릭터명을 입력하고 엔터', key=next(widget_id))    
+    characterName2 = st.text_input(label='캐릭터명2',value='캐릭터명을 입력하고 엔터', placeholder ='캐릭터명을 입력하고 엔터', key=next(widget_id))    
 
 
 charInfo1 = dict()
 charInfo2 = dict()
-if characterName and (characterName != "캐릭터명을 입력하세요."):
+if characterName and (characterName != "캐릭터명을 입력하고 엔터"):
     charInfo1 = getCharInfo(characterName)
 
-if characterName2 and (characterName2 != "캐릭터명을 입력하세요."):
+if characterName2 and (characterName2 != "캐릭터명을 입력하고 엔터"):
     charInfo2 = getCharInfo(characterName2)    
 
 
@@ -64,6 +64,14 @@ df1,df2 = (pd.DataFrame([{'스탯':stringToFloat(i['stat_value']),'스탯명':i[
            pd.DataFrame([{'스탯':stringToFloat(i['stat_value']),'스탯명':i['stat_name'].strip(),'캐릭터':characterName2}  for i in charInfo2.get('final_stat',[])])
            )
 df12 = pd.merge(df1,df2, on=['스탯명'],how='outer').fillna({'스탯_x':0.0,'스탯_y':0.0})
+if len(df12) == 0:
+    df12 = pd.DataFrame({'스탯_x':[0.0],
+                         '스탯_y':[0.0],
+                         '캐릭터_x':[''],
+                         '캐릭터_y':[''],
+                         '스탯명':[''],
+                         })
+
 df12['스탯율_x'] = [ i/(i+j) if (i+j) != 0.0 else 0.0 for i,j in zip(df12['스탯_x'],df12['스탯_y'])]
 df12['스탯율_y'] = [ j/(i+j) if (i+j) != 0.0 else 0.0 for i,j in zip(df12['스탯_x'],df12['스탯_y'])]
 df11 = df12[['스탯율_x','스탯_x','스탯명','캐릭터_x']]
@@ -86,7 +94,7 @@ bars = alt.Chart(source).mark_bar().encode(
 )
 
 text = alt.Chart(source).mark_text(dx=-15, dy=3, color='white').encode(
-    x=alt.X('sum(스탯):Q').stack('zero'),
+    x=alt.X('sum(스탯율):Q').stack('zero'),
     y=alt.Y('스탯명:N'),
     detail='캐릭터:N',
     text=alt.Text('sum(스탯):Q', format='.1f')
