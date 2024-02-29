e1b62e4ae48d25a684d4856ce0092fc762db9fa6
namyunwoo <namyunwoo@icloud.com>
2024-02-29 21:52:42 +0900

    nexon api test

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,11 +15,13 @@ if characterName:
     urlString_ocid = "https://open.api.nexon.com/maplestory/v1/id?character_name=" + characterName
     response_ocid = requests.get(urlString_ocid, headers = headers)
 
-    ocid = response_ocid.json()['ocid']
+    ocid = response_ocid.json().get('ocid','')
+    if ocid != "":
+        urlString_stat = 'https://open.api.nexon.com/maplestory/v1/character/stat?ocid={}&date={}'.format(ocid,(datetime.now()-timedelta(days=1)).strftime('%Y-%m-%d'))
 
-    urlString_stat = 'https://open.api.nexon.com/maplestory/v1/character/stat?ocid={}&date={}'.format(ocid,(datetime.now()-timedelta(days=1)).strftime('%Y-%m-%d'))
+        response_stat = requests.get(urlString_stat, headers = headers)
 
-    response_stat = requests.get(urlString_stat, headers = headers)
-
-    st.write(response_stat.json())
+        st.write(response_stat.json())
+    else:
+        st.write("캐릭터명 '{}' 를 찾지 못했습니다.".format(characterName))
 
